0ff18560b5beb389654a3bd755c00af790fcf3ee
Madhav Kumar <madhavrkumar@gmail.com>
2016-11-30 05:58:57 -0500

    added Trump and edited manifest to match
    Signed-off-by: Madhav Kumar <madhavrkumar@gmail.com>

--- Accurate Title/script.js
+++ Accurate Title/script.js
@@ -5,5 +5,6 @@ var node;
 while(node=walker.nextNode()){
   var text = node.nodeValue.replace(/[a]lt(ernative)?[- ][r]ight(er)*s?/ig, "neo nazis");
   text = text.replace(/[a]lt(ernative)?[- ][l]eft/ig, "neo-nazis that believe in climate change and smoke weed");
+  text = text.replace(/(\bdonald\b )?(J\.? )?\btrump\b/ig, "the elected fascist");
   node.nodeValue = text;
 }
